eef49a27d1924906bf0799828c80bf6aa41dd7c0
Lunlis <ilingin.prod@mail.ru>
2024-11-29 12:25:36 +0300

    003-add-ucfunc-in-code

--- Code.js
+++ Code.js
@@ -1,9 +1,19 @@
-const printSum = (a, b) => {
-    return console.log(a + b);
-};
+// const printSum = (a, b) => {
+//     return console.log(a + b);
+// };
 
-const a = 50;
-const b = 71;
+// const a = 50;
+// const b = 71;
 
 
-(sum(a,b));
+// (sum(a,b));
+
+function ucFunc (str) {
+    if (!str) return str;
+    let result = ``;
+    return result = str[0].toUpperCase() + str.slice(1);
+}
+
+let sentence = `!`;
+
+console.log(ucFunc(sentence));
